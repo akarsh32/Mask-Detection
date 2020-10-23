bfe5371d4a6cf3ac4e9f388101c0a4de8631ec9f
akarsh32 <akarshgupta32@gmail.com>
2020-10-23 10:23:32 +0530

    socket implementation trial

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response
 
-from face_recognition import MaskDetection
+from mask_detection import MaskDetection
 
 # initialize the mask detector
 md = MaskDetection()
@@ -15,15 +15,9 @@ def index(name=None):
 @app.route('/video_feed')
 def video_feed():
     # return the response generated along with the specific media type (mime type)
-    # start a thread that will perform motion detection
-    """
-    t = threading.Thread(target=md.detect_mask())
-    t.daemon = True
-    t.start()
-    """
     return Response(md.generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == '__main__':
     # start the flask app
-    app.run(debug=True, threaded=True, use_reloader=False)
+    app.run(debug=True)
--- utils/detector_utils.py
+++ utils/detector_utils.py
@@ -30,12 +30,12 @@ def load_inference_graph():
     print("> ====== Loading frozen graph into memory")
     detection_graph = tf.Graph()
     with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
-        sess = tf.Session(graph=detection_graph)
+        sess = tf.compat.v1.Session(graph=detection_graph)
     print(">  ====== Inference graph loaded.")
     return detection_graph, sess
 
